7c92b5af7a752dc53f6fc3deced9d9292e5a3598
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-11-02 15:51:50 -0300

    Finalizando setup front-end com next

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+interface HomeProps {
+  count: string,
+}
 
-export default function Home() {
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: {props.count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      count: data.pools,
+    }
+  }
 }
